c28dfc5b1ec9cd9e8b3ab5f13a366ab8ba121f02
keanghongtang-afk <keanghongtang@gmail.com>
2026-06-17 15:59:02 +0700

    .

--- backend/app/app.py
+++ backend/app/app.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://keanghongtang-afk.github.io",
-        "https://estatecam.netlify.app/",
+        "https://estatecam.netlify.app",
         "http://localhost:5174",
         "http://localhost:3000",
         "http://127.0.0.1:5173",
